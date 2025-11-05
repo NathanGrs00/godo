190844c0ae504498bbe164de30ea33d17362f1c4
NathanGRSDesktop <nathangeers00@gmail.com>
2025-11-06 00:17:24 +0100

    feat: implement MongoDB repository for Task with Create, GetByID and GetByUserID functions

---
+++ backend/internal/repository/mongo/task.go
@@ -0,0 +1,76 @@
+package mongo
+
+import (
+	"context"
+
+	"github.com/NathanGrs00/godo/internal/models"
+	_interface "github.com/NathanGrs00/godo/internal/repository/interface"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+// taskRepo represents the MongoDB implementation of the Task repository.
+type taskRepo struct {
+	collection *mongo.Collection
+}
+
+// NewTaskRepository creates a new instance of TaskRepository using the provided MongoDB database.
+func NewTaskRepository(db *mongo.Database) _interface.TaskRepository {
+	return &taskRepo{
+		collection: db.Collection("tasks")
+	}
+}
+
+// Create adds a new task to the MongoDB collection.
+// r is like self in Python, it refers to the instance of taskRepo.
+func (r *taskRepo) Create(ctx context.Context, task *models.Task) error {
+	_, err := r.collection.InsertOne(ctx, task)
+	return err
+}
+
+// GetByID retrieves a task by its ID from the MongoDB collection.
+func (r *taskRepo) GetByID(ctx context.Context, id primitive.ObjectID) (*models.Task, error) {
+	// task will hold the result of the query
+	var task models.Task
+	// Filter to find the task by its ID
+	filter := primitive.M{"_id": id}
+	// Perform the query and decode the result into the task variable
+	err := r.collection.FindOne(ctx, filter).Decode(&task)
+	// Return nil for the task and the error if there was an issue
+	if err != nil {
+		return nil, err
+	}
+	// Return the found task
+	return &task, nil
+}
+
+// GetByUserID retrieves all tasks for a specific user from the MongoDB collection.
+func (r *taskRepo) GetByUserID(ctx context.Context, userID primitive.ObjectID) ([]*models.Task, error) {
+	// Filter to find tasks by user ID
+	filter := primitive.M{"user_id": userID}
+
+	cursor, err := r.collection.Find(ctx, filter)
+	if err != nil {
+		return nil, err
+	}
+	// Ensure the cursor is closed after processing
+	defer cursor.Close(ctx)
+
+	// tasks will hold the list of tasks for the user
+	var tasks []*models.Task
+	// Iterate through the cursor
+	for cursor.Next(ctx) {
+		// Decode each document into a Task struct
+		var task models.Task
+		// If error occurs during decoding, return it
+		if err := cursor.Decode(&task); err != nil {
+			return nil, err
+		}
+		// Append the task to the tasks slice
+		tasks = append(tasks, &task)
+	}
+
+	// Return the list of tasks
+	return tasks, nil
+}
